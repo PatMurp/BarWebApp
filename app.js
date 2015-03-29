72f5535670b13bc4202304585f42484a425c4b45
Patrick Murphy <murphypatv@gmail.com>
2015-03-29 22:17:40 +0100

    fix layout bug in events partial

--- app.js
+++ app.js
@@ -72,11 +72,11 @@ function run($rootScope, $location, $cookieStore, $http) {
 	$rootScope.$on('$locationChangeStart', function(event, next, current) {
 		// redirect to login page if not logged in and trying to access a restricted page
 		var restrictedPage = $.inArray($location.path(), [
-			'/login', '/register', '/adminLogin', '/home', '/menu', '/events', '/gallery'
+			'/login', '/register', '/adminLogin', '/home', '/menu', '/events'
 			]) === -1;
 		var loggedIn = $rootScope.globals.currentUser;
 		if (restrictedPage && !loggedIn) {
-			$location.path('/adminLogin');
+			$location.path('/home');
 		}
 	});
 }
