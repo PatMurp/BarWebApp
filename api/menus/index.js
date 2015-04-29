d27bd38f7cdc49f0c04874c6bf4785482510bb8a
Patrick Murphy <murphypatv@gmail.com>
2015-04-29 18:56:57 +0100

    update menu items with API

--- api/menus/index.js
+++ api/menus/index.js
@@ -8,6 +8,12 @@ router.get('/starters', controller.showStarters);
 router.get('/mains', controller.showMains);
 router.get('/deserts', controller.showDeserts);
 router.get('/wines', controller.showWines);
+
 router.post('/starters', controller.createStarter);
+
+router.put('/starters/:id', controller.updateStarter);
+router.put('/mains/:id', controller.updateMain);
+router.put('/deserts/:id', controller.updateDesert);
+router.put('/wines/:id', controller.updateWine);
 
 module.exports = router;
--- api/menus/menus.controller.js
+++ api/menus/menus.controller.js
@@ -6,22 +6,27 @@ exports.index = function(req, res) {
 	return res.json(200, datastore.menus);
 };
 
+// get list of starters
 exports.showStarters = function(req, res) {
 	return res.json(200, datastore.menus[0].starters);
 }
 
+// get list of mains
 exports.showMains = function(req, res) {
   return res.json(200, datastore.menus[0].mains);
 }
 
+// get list of deserts
 exports.showDeserts = function(req, res) {
   return res.json(200, datastore.menus[0].deserts);
 }
 
+// get list of wines
 exports.showWines = function(req, res) {
   return res.json(200, datastore.menus[0].wines);
 }
 
+// create new starter in datastore
 exports.createStarter = function(req, res) {
     var nextId = 0
     var last = _.last(datastore.menus[0].starters)
@@ -39,6 +44,74 @@ exports.createStarter = function(req, res) {
     };
     datastore.menus[0].starters.push(starter)
     return res.json(201, starter);
+};
+
+// update existing starter in datastore
+exports.updateStarter = function(req, res) {
+	var index = _.findIndex(datastore.menus[0].starters,
+		function(starter) {
+			return starter.id == req.params.id;
+		});
+	if (index != -1) {
+		var starter = datastore.menus[0].starters[index]
+		starter.name = req.body.name
+		starter.description = req.body.description
+		starter.price = req.body.price
+		return res.send(200, starter)
+	} else {
+		return res.send(404)
+	}
+};
+
+// update existing starter in datastore
+exports.updateMain = function(req, res) {
+	var index = _.findIndex(datastore.menus[0].mains,
+		function(main) {
+			return main.id == req.params.id;
+		});
+	if (index != -1) {
+		var main = datastore.menus[0].mains[index]
+		main.name = req.body.name
+		main.description = req.body.description
+		main.price = req.body.price
+		return res.send(200, main)
+	} else {
+		return res.send(404)
+	}
+};
+
+// update existing deserts in datastore
+exports.updateDesert = function(req, res) {
+	var index = _.findIndex(datastore.menus[0].deserts,
+		function(desert) {
+			return desert.id == req.params.id;
+		});
+	if (index != -1) {
+		var desert = datastore.menus[0].deserts[index]
+		desert.name = req.body.name
+		desert.description = req.body.description
+		desert.price = req.body.price
+		return res.send(200, desert)
+	} else {
+		return res.send(404)
+	}
+};
+
+// update existing wines in datastore
+exports.updateWine = function(req, res) {
+	var index = _.findIndex(datastore.menus[0].wines,
+		function(wine) {
+			return wine.id == req.params.id;
+		});
+	if (index != -1) {
+		var wine = datastore.menus[0].wines[index]
+		wine.name = req.body.name
+		wine.description = req.body.description
+		wine.price = req.body.price
+		return res.send(200, wine)
+	} else {
+		return res.send(404)
+	}
 };
 
 
